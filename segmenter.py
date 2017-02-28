5b082909de63d78445c717446ad86b811620d633
Brian McFee <brian.mcfee@nyu.edu>
2017-02-28 15:18:46 -0500

    added more passes on k-means

--- segmenter.py
+++ segmenter.py
@@ -87,7 +87,7 @@ def embed_beats(A_rep, A_loc):
 def cluster(evecs, Cnorm, k, beat_times):
     X = evecs[:, :k] / Cnorm[:, k-1:k]
 
-    KM = sklearn.cluster.KMeans(n_clusters=k)
+    KM = sklearn.cluster.KMeans(n_clusters=k, n_init=50, max_iter=500)
 
     seg_ids = KM.fit_predict(X)
 
